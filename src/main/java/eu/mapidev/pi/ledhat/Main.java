072669e16d5d568fd7961ad561a1797eff8f1618
Mark <mpietryga93@gmail.com>
2019-01-11 21:14:38 +0100

    Add startup class

---
+++ src/main/java/eu/mapidev/pi/ledhat/Main.java
@@ -0,0 +1,18 @@
+package eu.mapidev.pi.ledhat;
+
+import com.github.mbelling.ws281x.LedStripType;
+import com.github.mbelling.ws281x.Ws281xLedStrip;
+import eu.mapidev.pi.ledhat.config.LedHatAdapter;
+
+public class Main {
+    
+    public static void main(String[] args) {
+	if (args.length < 1) {
+	    System.out.println("usage: (program) color_can_be_hex");
+	    System.exit(0);
+	}
+	
+	Ws281xLedStrip wls = new Ws281xLedStrip(32, 18, 800000, 5, 100, 0, false, LedStripType.WS2811_STRIP_GRB, true);
+	StripLighter stripLighter = new StripLighter(new LedHatAdapter(wls));
+    }
+}
