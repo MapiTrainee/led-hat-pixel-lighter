a921ff84a11e9e765918537c6f16b334d057ae60
Mark <mpietryga93@gmail.com>
2019-01-12 16:26:24 +0100

    Add option to set color on startup

--- src/main/java/eu/mapidev/pi/ledhat/Main.java
+++ src/main/java/eu/mapidev/pi/ledhat/Main.java
@@ -3,6 +3,7 @@ package eu.mapidev.pi.ledhat;
 import com.github.mbelling.ws281x.LedStripType;
 import com.github.mbelling.ws281x.Ws281xLedStrip;
 import eu.mapidev.pi.ledhat.config.LedHatAdapter;
+import eu.mapidev.pi.ledhat.util.ColorParser;
 
 public class Main {
     
@@ -12,7 +13,9 @@ public class Main {
 	    System.exit(0);
 	}
 	
+	ColorParser cp = new ColorParser();
 	Ws281xLedStrip wls = new Ws281xLedStrip(32, 18, 800000, 5, 100, 0, false, LedStripType.WS2811_STRIP_GRB, true);
 	StripLighter stripLighter = new StripLighter(new LedHatAdapter(wls));
+	stripLighter.light(cp.parse(args[0]));
     }
 }
